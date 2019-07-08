b22f340d7740d5d0f960c1c831975bfcc4bf08e9
EXComm19 <excomm19@icloud.com>
2019-07-09 00:28:22 +1000

    Added Dev tool and dev tool test

---
+++ Commit.ps1
@@ -0,0 +1,15 @@
+$message = Read-Host 'Commit Message';
+$version = Read-Host 'Patch(0), Minor(1) or Major(2)'
+
+if ($version -eq 0) {
+    npm version patch;
+} elseif ($version -eq 1) {
+    npm version minor;
+} elseif ($version -eq 2) {
+    npm version major
+} else {
+    npm version patch;
+}
+
+git add .
+git commit -m $message
